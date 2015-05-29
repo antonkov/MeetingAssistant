24a5a841cbe97a3b45f357f8b3cef7e5c845e683
Grigory Tkachenko <grtkachenko@gmail.com>
2015-05-30 02:42:30 +0300

    Added actions in toolbar

--- Android/app/src/main/java/com/gtdev/meetingassistant/AddEventActivity.java
+++ Android/app/src/main/java/com/gtdev/meetingassistant/AddEventActivity.java
@@ -3,15 +3,37 @@ package com.gtdev.meetingassistant;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class AddEventActivity extends Activity {
+public class AddEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("New meeting");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddEventActivity.this.onBackPressed();
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_add_event, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
--- Android/app/src/main/java/com/gtdev/meetingassistant/MainActivity.java
+++ Android/app/src/main/java/com/gtdev/meetingassistant/MainActivity.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.melnykov.fab.FloatingActionButton;
@@ -22,12 +26,24 @@ import jp.wasabeef.recyclerview.animators.adapters.AnimationAdapter;
 import jp.wasabeef.recyclerview.animators.adapters.ScaleInAnimationAdapter;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Meetings");
+        setSupportActionBar(toolbar);
+
         final FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +82,7 @@ public class MainActivity extends Activity {
             }
         });
         swipeLayout.setColorSchemeResources(android.R.color.holo_red_light);
+
 
     }
 }
