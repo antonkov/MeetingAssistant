a2e9a91c20712f50ccfad0d521453635ed31232d
Grigory Tkachenko <grtkachenko@gmail.com>
2015-05-30 04:26:31 +0300

    Improved AddEventActivity

--- Android/app/src/main/java/com/gtdev/meetingassistant/AddEventActivity.java
+++ Android/app/src/main/java/com/gtdev/meetingassistant/AddEventActivity.java
@@ -6,9 +6,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.TextView;
+
+import com.fourmob.datetimepicker.date.DatePickerDialog;
+import com.sleepbot.datetimepicker.time.RadialPickerLayout;
+import com.sleepbot.datetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
 
 
-public class AddEventActivity extends AppCompatActivity {
+public class AddEventActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    public static final String DATEPICKER_TAG = "datepicker";
+    public static final String TIMEPICKER_TAG = "timepicker";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,30 @@ public class AddEventActivity extends AppCompatActivity {
                 AddEventActivity.this.onBackPressed();
             }
         });
+
+
+        final Calendar calendar = Calendar.getInstance();
+
+        final DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), false);
+        final TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false, false);
+
+        findViewById(R.id.dateTextView).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.setYearRange(1985, 2028);
+                datePickerDialog.setCloseOnSingleTapDay(false);
+                datePickerDialog.show(getSupportFragmentManager(), DATEPICKER_TAG);
+            }
+        });
+
+        findViewById(R.id.timeTextView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.setCloseOnSingleTapMinute(false);
+                timePickerDialog.show(getSupportFragmentManager(), TIMEPICKER_TAG);
+            }
+        });
     }
 
     @Override
@@ -33,4 +67,13 @@ public class AddEventActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+        ((TextView) findViewById(R.id.dateTextView)).setText("" + day + "/" + month + "/" + year);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        ((TextView) findViewById(R.id.timeTextView)).setText("" + hourOfDay + ":" + minute);
+    }
 }
